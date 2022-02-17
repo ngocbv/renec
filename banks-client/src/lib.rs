84d44b86998d696dcc34519c00df139dcb61d91e
ngocbv <ngocbv@remitano.com>
2022-02-17 16:17:25 +0700

    Rebuild renec-banks-interface package

--- banks-client/src/lib.rs
+++ banks-client/src/lib.rs
@@ -5,11 +5,11 @@
 //! but they are undocumented, may change over time, and are generally more
 //! cumbersome to use.
 
-pub use solana_banks_interface::{BanksClient as TarpcClient, TransactionStatus};
+pub use renec_banks_interface::{BanksClient as TarpcClient, TransactionStatus};
 use {
     borsh::BorshDeserialize,
     futures::{future::join_all, Future, FutureExt},
-    solana_banks_interface::{BanksRequest, BanksResponse},
+    renec_banks_interface::{BanksRequest, BanksResponse},
     solana_program::{
         clock::{Clock, Slot},
         fee_calculator::FeeCalculator,
