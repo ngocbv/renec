06a2e02b4499f52006271ddc33044c0ef42a4d57
ngocbv <ngocbv@remitano.com>
2022-02-17 22:52:17 +0700

    Rebuild renec-notifier

--- watchtower/src/main.rs
+++ watchtower/src/main.rs
@@ -11,7 +11,7 @@ use {
     renec_cli_output::display::format_labeled_address,
     renec_client::{client_error, rpc_client::RpcClient, rpc_response::RpcVoteAccountStatus},
     solana_metrics::{datapoint_error, datapoint_info},
-    solana_notifier::Notifier,
+    renec_notifier::Notifier,
     solana_sdk::{
         hash::Hash,
         native_token::{sol_to_lamports, Sol},
